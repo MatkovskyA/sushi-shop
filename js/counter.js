81e5610ab086a49a64132fd88cf6795c2783cefa
MatkovskyA <a.matkovsky14@gmail.com>
2024-05-22 09:19:59 +0300

    Added html for project

---
+++ js/counter.js
@@ -0,0 +1,9 @@
+const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+
+btnMinus.addEventListener('click', () => {
+  console.log("Minus")
+})
+btnPlus.addEventListener('click', () => {
+  console.log("Plus")
+})
