2a8351b7302a3364af800a79613105a7e835b6e9
MatkovskyA <a.matkovsky14@gmail.com>
2024-05-22 09:36:37 +0300

    added logic for the product card counter

--- js/counter.js
+++ js/counter.js
@@ -1,9 +1,14 @@
+// Находим элементы счетчика карточки товаров 
 const btnMinus = document.querySelector('[data-action="minus"]');
 const btnPlus = document.querySelector('[data-action="plus"]');
+const counter = document.querySelector('[data-counter]')
 
+// Клик на кнопку - и +
 btnMinus.addEventListener('click', () => {
-  console.log("Minus")
+  parseInt(counter.innerText) > 1 ? --counter.innerText : 1;
 })
+
 btnPlus.addEventListener('click', () => {
-  console.log("Plus")
+  counter.innerText = ++counter.innerText;
 })
+
