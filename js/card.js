de10d775de2e9f5edfc6c844117c7ff7cab1d86c
MatkovskyA <a.matkovsky14@gmail.com>
2024-05-25 22:15:48 +0300

    Added logic for combining identical product

--- js/card.js
+++ js/card.js
@@ -13,35 +13,40 @@ window.addEventListener('click', (event) => {
       price: card.querySelector('.price__currency').innerText,
       counter: card.querySelector('[data-counter]').innerText,
     };
-    console.log(productInfo)
 
-    const cartItemSample = `
-    <div class="cart-item" data-id=${productInfo.id}>
-      <div class="cart-item__top">
-        <div class="cart-item__img">
-          <img src=${productInfo.imgSrc} alt="">
-        </div>
-        <div class="cart-item__desc">
-          <div class="cart-item__title">${productInfo.title}</div>
-          <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
-          <!-- cart-item__details -->
-          <div class="cart-item__details">
-            <div class="items items--small counter-wrapper">
-              <div class="items__control" data-action="minus">-</div>
-              <div class="items__current" data-counter="">${productInfo.counter}</div>
-              <div class="items__control" data-action="plus">+</div>
+    const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`)
+
+    if(itemInCart) {
+      const counterElem = itemInCart.querySelector("[data-counter]")
+      counterElem.innerText = parseInt(counterElem.innerText) + parseInt(productInfo.counter)
+    } else {
+      cartItemSample = `
+        <div class="cart-item" data-id=${productInfo.id}>
+          <div class="cart-item__top">
+            <div class="cart-item__img">
+              <img src=${productInfo.imgSrc} alt="">
             </div>
-            <div class="price">
-              <div class="price__currency">${productInfo.price}</div>
+            <div class="cart-item__desc">
+              <div class="cart-item__title">${productInfo.title}</div>
+              <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+              <!-- cart-item__details -->
+              <div class="cart-item__details">
+                <div class="items items--small counter-wrapper">
+                  <div class="items__control" data-action="minus">-</div>
+                  <div class="items__current" data-counter="">${productInfo.counter}</div>
+                  <div class="items__control" data-action="plus">+</div>
+                </div>
+                <div class="price">
+                  <div class="price__currency">${productInfo.price}</div>
+                </div>
+              </div>
+              <!-- // cart-item__details -->
             </div>
           </div>
-          <!-- // cart-item__details -->
         </div>
-      </div>
-    </div>
-  `
+      `
   
   cartWrapper.insertAdjacentHTML("beforeend", cartItemSample)
-  }
+  }}
 })
 
